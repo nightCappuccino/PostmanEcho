5452c404433fdb076daade2b6e067598b58eb9f1
Julia Novikova <julikova.n@gmail.com>
2023-07-15 15:11:52 +0300

    add Test mistake

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -24,7 +24,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo( "Привет, Postman!"))
+                .body("data", equalTo( "Hello, Postman!"))
         ;
     }
 
